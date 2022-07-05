ee6e72a66defb023573a9ba897a6975b922fc2a1
Kaushik Chowdhury <95243692+darkard2003@users.noreply.github.com>
2022-07-05 23:33:13 +0530

    better implimentation than before

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 FILENAME = 'webpage'
 FILETYPE = 'html'
+REPLACE_DOUBLEQUOTE = True
+
 
 with open(f'./input/{FILENAME}.{FILETYPE}', 'r') as file:
-    data = file.read()
+    data = file.read().split('\n')
 
+def onLine():
+    with open(f'./output/{FILENAME}.txt', 'w') as output:
+        for line in data:
+            line = line.lstrip()
+            for word in line:
+                if REPLACE_DOUBLEQUOTE:
+                    if word == '"':
+                        print(word)
+                        word = f'\{word}'
+                        print(word)
+                output.write(word)
+        
 
-with open(f'./output/{FILENAME}.txt', 'w') as output:
-    for line in data:
-        line = line.lstrip()
-        for word in line:
-            output.write(word)
+if __name__ == '__main__':
+    onLine()
+    
